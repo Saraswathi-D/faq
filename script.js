f973166d151ee8dbd35276408ee370fea151e341
Saraswathi D <saraswathidurairaj123@gmail.com>
2026-08-20 14:43:26 +0530

    Initial FAQ project

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 const questions = document.querySelectorAll(".question");
+const buttons = document.querySelectorAll(".btn");
+const faqItems = document.querySelectorAll(".faq-item");
 
 questions.forEach(function(question) {
-
     question.addEventListener("click", function() {
-
         const faqItem = question.parentElement;
-
         faqItem.classList.toggle("active");
+    });
+});
 
+function showCategory(selectedCategory) {
+    buttons.forEach(function(button) {
+        button.classList.toggle(
+            "active",
+            button.getAttribute("data") === selectedCategory
+        );
     });
 
+    faqItems.forEach(function(item) {
+        const isSelected = item.getAttribute("data") === selectedCategory;
+
+        item.style.display = isSelected ? "block" : "none";
+
+        if (!isSelected) {
+            item.classList.remove("active");
+        }
+    });
+}
+
+buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        showCategory(button.getAttribute("data"));
+    });
 });
+
+showCategory("java");
